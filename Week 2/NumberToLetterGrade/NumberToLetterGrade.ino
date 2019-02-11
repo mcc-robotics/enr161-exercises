bcb78fb680ed8f439c21ffc66e649753ad56e8b2
Geoff Berl <gberl001@gmail.com>
2019-02-11 09:41:10 -0500

    Adding a control structures exercise

---
+++ Week 2/NumberToLetterGrade/NumberToLetterGrade.ino
@@ -0,0 +1,41 @@
+
+
+
+void setup() {
+  // Setup serial monitor
+  Serial.begin(9600);
+
+  // Create some local variables
+  char letterGrade = ' ';     // Stores the resulting letter grade
+  char numericalGrade = 75;   // Stores the numerical grade
+
+  // Convert from a numerical grade to a letter grade
+  if numericalGrade >= 90 {
+    letterGrade = 'A'
+  }
+  if numericalGrade >= 80 {
+    letterGrade = 'B'
+  }
+  if numericalGrade >= 70 {
+    letterGrade = 'C'
+  }
+  if numericalGrade >= 60 {
+    letterGrade = 'D'
+  }
+  if numericalGrade < 60 {
+    letterGrade = 'F'
+  }
+
+  // Print out our results
+  Serial.print("The letter grade for a ");
+  Serial.print(numericalGrade);
+  Serial.print(" is ");
+  Serial.print(letterGrade);
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
