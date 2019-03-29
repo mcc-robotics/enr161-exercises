c153cea7e1453f92f80e3e90173f7e09327ba7c5
Geoff Berl <gberl001@gmail.com>
2019-03-29 18:56:52 -0400

    Adding swap example for Ch7 homework

---
+++ Chapter_7/Swap/Swap.ino
@@ -0,0 +1,48 @@
+/* Swap
+ *
+ * Homework Problem - Implement the swap function so the output is correct
+ *
+ * Author:
+ * Assisted by:
+ * Date:
+ */
+
+
+// ************************* NOTICE *****************************
+// **************************************************************
+// ******** Only write code in the swap() function **************
+// **************************************************************
+// **************************************************************
+
+void setup() {
+  Serial.begin(115200);
+  int thisValue = 10;
+  int thatValue = 50;
+
+  printThisAndThat(thisValue, thatValue);
+  Serial.println("------^ Before Swap ^--------------\n");
+
+  Serial.println("Calling swap(thisValue, thatValue)\n");
+  swap(thisValue, thatValue);
+
+  Serial.println("------_ After Swap _-------------");
+  printThisAndThat(thisValue, thatValue);
+
+
+}
+
+void loop() {
+
+}
+
+void printThisAndThat(int thisValue, int thatValue) {
+  Serial.print("thisValue is ");
+  Serial.println(thisValue);
+  Serial.print("thatValue is ");
+  Serial.println(thatValue);
+}
+
+void swap(int &a, int &b) {
+  // Write your code here'
+
+}
